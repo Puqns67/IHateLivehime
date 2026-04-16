b2a1c751243a03d8f5df2469947998cb92d7f788
Puqns67 <me@puqns67.icu>
2026-04-16 22:19:20 +0800

    Bump version to 0.1.9

--- ihatelivehime.meta.js
+++ ihatelivehime.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        IHateLivehime
-// @name:zh-CN  我讨厌直播姬'
+// @name:zh-CN  我讨厌直播姬
 // @namespace   https://github.com/Puqns67
-// @version     0.1.8
+// @version     0.1.9
 // ==/UserScript==
